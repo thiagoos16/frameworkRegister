08b482dc318a8cc88b7198a0777e9e3dbf9c0808
thiagometal <thiagometal.fne@gmail.com>
2018-07-19 15:32:57 -0400

    add fields in framework model

--- app/Framework.php
+++ app/Framework.php
@@ -8,10 +8,10 @@ class Framework extends Model
 {
     protected $table = 'framework';
 
-    protected $fillable = ['name', 'url_name', 'site', 'year_creation', 'creator', 
-                            'latest_stable_release', 'type', 'opinion', 'pros_cons'];
+    protected $fillable = ['name', 'url_image', 'site', 'year_creation', 'creator', 
+                            'latest_stable_release', 'type', 'opinion', 'pros_cons', 'id_language'];
 
     public function programmingLanguage() {
-        return $this->belongTo('App\ProgrammingLanguage');
+        return $this->belongTo('App\ProgrammingLanguage', 'id_language');
     }
 }
